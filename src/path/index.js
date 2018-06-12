af4d35eaec8ad5aae3209c6725cd662d7efd0b44
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-06-12 14:31:32 -0400

    Allow DotNotaion for Path

--- src/path/index.js
+++ src/path/index.js
@@ -3,6 +3,10 @@ import { reduceValues_ } from "../reduceValues"
 import { curry2 } from "../curry"
 
 export const path_ = (keychain, tree) => {
+  if (typeof keychain === "string") {
+    keychain = keys.trim().split(",")
+  }
+  
   return reduceValues_((acc, val) => prop_(val, acc), tree, keychain)
 }
 
