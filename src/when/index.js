31f9cbc8eb4a604f61624ed32c3410927467afb1
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-06-18 10:46:28 -0400

    Fix When and add Test

--- src/when/index.js
+++ src/when/index.js
@@ -1,3 +1,3 @@
-import curry2_ from '../_uncurried/curry2_'
+import curry3_ from '../_uncurried/curry3_'
 import when_ from '../_uncurried/when_'
-export default curry2_(when_)
+export default curry3_(when_)
--- src/when/index.spec.js
+++ src/when/index.spec.js
@@ -1,11 +1,13 @@
-import PLACEHOLDER from "./index";
+import when from ".";
 
-describe.skip("PLACEHOLDER", () => {
-  it("Subtract One from Number", () => {
-    const a = PLACEHOLDER(1);
-    const eA = 0;
+describe("When", () => {
+  var getEven = when(i => i % 2 === 1, i => i + 1)
 
-    expect(a).toEqual(eA);
-  });
+  test('applies callback if condition is true', () => {
+    expect(getEven(1)).toBe(2)
+  })
 
+  test('returns passed value if condition is false', () => {
+    expect(getEven(2)).toBe(2)
+  })
 });
