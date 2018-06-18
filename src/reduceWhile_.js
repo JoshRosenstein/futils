ef4e9869266c41663428b9741ce1d1eb921b7137
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-06-18 13:09:08 -0400

    Fix ReduceWhile for multi types

--- src/reduceWhile_.js
+++ src/reduceWhile_.js
@@ -3,22 +3,27 @@ import type_ from "./type_"
 
 export default (pred,reducer, initial, functor) => {
   let fn
+ let predfn
   switch (type_(functor)) {
     case "Array":
       fn = (acc, value, key) => reducer(acc, value, key)
+      predfn = (acc, value, key) => pred(acc, value, key)
       break
     case "Object":
     case "Map":
       fn = (acc, [key, value]) => reducer(acc, value, key)
       functor = toPairs_(functor)
+      predfn = (acc, [key, value]) => pred(acc, value, key)
       break
     case "Set":
       fn = (acc, [, value]) => reducer(acc, value)
       functor = toPairs_(functor)
+      predfn = (acc, [, value]) => pred(acc, value)
       break
     case "String":
       fn = (acc, [key, value]) => reducer(acc, value, key)
       functor = toPairs_(functor.split(""))
+       predfn = (acc, [key, value]) => pred(acc, value, key)
       break
 
     default: {
@@ -28,10 +33,15 @@ export default (pred,reducer, initial, functor) => {
     }
   }
 
-  let res = initial
-  let copy = [].concat(functor)
-  while (copy.length && pred(res, copy[0])) res = fn(res, copy.shift())
-  return res
+  const length = functor.length;
+  let b = initial;
 
+  for (let i = 0; i < length; ++i) {
+    const a = functor[i];
+    if (!predfn(b, a, i)) break;
 
+    b = fn(b, a, i);
+  }
+
+  return b;
 }
