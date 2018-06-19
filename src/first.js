c5602edf3b95d9d0153d879f609fc44643bf63cb
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-06-19 00:27:46 -0400

    Add First

---
+++ src/first.js
@@ -0,0 +1,5 @@
+
+
+import first_ from './first_'
+
+export default first_
---
+++ src/first.spec.js
@@ -0,0 +1,12 @@
+import first from "./first";
+
+describe("first", () => {
+  test('returns first element of array', () => {
+    expect(first([1, 2, 3])).toBe(1)
+  })
+
+  test('returns first element of string', () => {
+    expect(first('hello')).toBe('h')
+  })
+
+});
