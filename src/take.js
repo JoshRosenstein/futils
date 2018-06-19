c357dce71d99fd635d9367e6a8ad21b906c368f1
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-06-19 00:25:17 -0400

    Add Take

---
+++ src/take.js
@@ -0,0 +1,4 @@
+import take_ from './take_'
+import curry2_ from './curry2_'
+
+export default curry2_(take_)
---
+++ src/take_.js
@@ -0,0 +1,7 @@
+import gt_ from "./gt_"
+import append_ from "./append_"
+import reduce_ from "./reduce_"
+import empty_ from "./empty_"
+
+
+export default (count,orderedList) => count<0 ? orderedList :reduce_((acc,v,idx) =>gt_(idx,count - 1)?acc:append_(v,acc),empty_(orderedList),orderedList)
