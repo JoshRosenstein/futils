e92a4e2cd1c0fc104ad80e66de6d0ecdc81da45e
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-07-16 17:20:23 -0400

    Add Group By, fix propOr

---
+++ src/groupBy.js
@@ -0,0 +1,5 @@
+import groupBy_ from './groupBy_'
+import curry2_ from './curry2_'
+
+
+export default curry2_(groupBy_)
---
+++ src/propOr.js
@@ -0,0 +1,3 @@
+import curry3_ from './curry3_'
+import propOr_ from './propOr_'
+export default curry3_(propOr_)
