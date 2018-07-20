8ce58e47bd7e578fcddafee92d4228bb980a59dd
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-07-20 12:34:25 -0400

    missed DispatchWith

---
+++ src/dispatchWith_.js
@@ -0,0 +1,17 @@
+
+import converge_ from './converge_'
+import compose_ from './compose_'
+import juxt_ from './juxt_'
+import cond from './cond'
+import map from './map'
+import last from './last'
+import apply from './apply'
+import call from './call'
+import xPairs from './xPairs'
+
+
+const getPredicates = compose_(map(juxt_([apply(compose_), last])),xPairs)
+
+const dispatchWith = converge_(call(cond), [getPredicates])
+
+export default dispatchWith
