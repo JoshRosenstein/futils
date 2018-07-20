69914699d61126267163964f2fb1ec605d2638dc
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-07-20 12:39:28 -0400

    Fix sequence map

--- src/sequence_.js
+++ src/sequence_.js
@@ -1,4 +1,4 @@
-import mapValues from './mapvalues'
+import map_ from './map_'
 import applyTo_ from './applyTo_'
 
-export default (fns,value)=>mapValues(fn=>applyTo_(value,fn),fns)
+export default (fns,value)=>map_(fn=>applyTo_(value,fn),fns)
