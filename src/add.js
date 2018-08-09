98155ebc2d153866392b3a6934baeb2d83264186
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-08-09 17:17:11 -0400

    add ADD function

---
+++ src/add.js
@@ -0,0 +1,4 @@
+import curry2_ from './curry2_'
+import add_ from './add_'
+
+export default curry2_(add_)
---
+++ src/add.spec.js
@@ -0,0 +1,11 @@
+import add from './add'
+
+describe('add', () => {
+  it('adds two numbers', () =>
+    expect(add(1, 2)).toEqual(3)
+  )
+
+  it('is curried', () =>
+    expect(add(1)(2)).toEqual(3)
+  )
+})
