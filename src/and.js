3406d22c253bf5cd01f32e6e50f9f6dfe2f76ae2
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-08-09 17:07:16 -0400

    Add and

---
+++ src/and.js
@@ -0,0 +1,4 @@
+import curryN from './curryN'
+import and_ from './and_'
+
+export default curryN(2,and_)
---
+++ src/and.spec.js
@@ -0,0 +1,10 @@
+import and from './and'
+
+describe('And', () => {
+  test('returns value given in 1st argument', () => {
+    expect(and(true, true)).toBeTruthy()
+    expect(and(true, false)).toBeFalsy()
+    expect(and(false, true)).toBeFalsy()
+    expect(and(false, false)).toBeFalsy()
+  })
+})
