8d486a64e9555b3e21666eafbc12ade009154f44
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-10-03 05:41:29 -0400

    Add Paths

---
+++ src/paths.js
@@ -0,0 +1,4 @@
+import curry2_ from './curry2_'
+import paths_ from './paths_'
+
+export default curry2_(paths_)
---
+++ src/paths.spec.js
@@ -0,0 +1,24 @@
+import paths from './paths'
+
+describe('paths', () => {
+  test('getMany with Object', () => {
+    expect(paths(['x', 'y'], { x: 1, y: 2 })).toEqual([1, 2])
+    expect(paths(['x.a', 'y.a'], { x: {a:1}, y: {a:2} })).toEqual([1, 2])
+  })
+
+  test('Array Comma', () => {
+    expect(paths('0,1', [1, 2])).toEqual([1, 2])
+    expect(paths('0.0,1.0', [[1], [2]])).toEqual([1, 2])
+  })
+
+  test('Array', () => {
+    expect(paths([1, 2], [1, 2, 3])).toEqual([2, 3])
+    expect(paths([['1','0'], ['2','0']], [[1,11], [2,22], [3,33] ])).toEqual([2, 3])
+    expect(paths([[1,0], [2,0]], [[1,11], [2,22], [3,33] ])).toEqual([2, 3])
+  })
+
+  test('Splits Comma Seperated Strings', () => {
+    expect(paths('x,y', { x: 1, y: 2 })).toEqual([1, 2])
+    expect(paths('x.a,y.a', { x: {a:1}, y: {a:2} })).toEqual([1, 2])
+  })
+})
