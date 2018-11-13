7752fcef78511df68b0a685ae663af7182381da8
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-11-13 13:28:12 -0500

    ✨ types(identity): add

---
+++ src/identity.d.ts
@@ -0,0 +1,8 @@
+ // ROSEYS TS
+
+export declare type Identity_ = <T>(value: T)=> T
+export declare type Identity =Identity_
+
+export declare var identity_: Identity_
+export declare var identity: Identity
+export default identity
