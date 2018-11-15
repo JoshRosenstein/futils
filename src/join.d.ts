d154cd4efae9deab3f38052304d0f040e081b0cd
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-11-14 23:55:51 -0500

    ✨ types(join)

---
+++ src/join.d.ts
@@ -0,0 +1,13 @@
+ // ROSEYS TS
+
+
+export declare type Join_ = {
+  (separator: string, list: any[]): string
+}
+export declare type Join ={
+  (separator: string):(list: any[])=>string
+(separator: string, list: any[]): string
+}
+export declare var join_: Join_
+export declare var join: Join
+export default join
