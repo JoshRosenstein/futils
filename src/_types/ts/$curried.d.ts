9b75e65d3b512de64dbc38bd4027f9e41aa6d324
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-11-15 00:16:19 -0500

    💤 chore: settings

--- src/_types/ts/$curried.d.ts
+++ src/_types/ts/$curried.d.ts
@@ -32,20 +32,21 @@ export interface Curried5<A1, A2, A3, A4, A5, R> {
   (a1: A1, a2: A2, a3: A3, a4: A4): Curried1<A5, R>;
   (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, ...args: any[]): R;
 }
-
+export interface Curried6<A1, A2, A3, A4, A5,A6, R> {
+  (): Curried6<A1, A2, A3, A4, A5,A6, R>;
+  (a1: A1): Curried5<A2, A3, A4, A5, A6, R>;
+  (a1: A1, a2: A2): Curried4<A3, A4, A5,A6, R>;
+  (a1: A1, a2: A2, a3: A3): Curried3<A4, A5,A6, R>;
+  (a1: A1, a2: A2, a3: A3, a4: A4): Curried2<A5,A6, R>;
+  (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5): Curried1<A6, R>;
+  (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6:A6, ...args: any[]): R;
+}
 export interface Curried {
   <A1, R>(func: (a1: A1) => R): Curried1<A1, R>;
   <A1, A2, R>(func: (a1: A1, a2: A2) => R): Curried2<A1, A2, R>;
   <A1, A2, A3, R>(func: (a1: A1, a2: A2, a3: A3) => R): Curried3<A1, A2, A3, R>;
   <A1, A2, A3, A4, R>(func: (a1: A1, a2: A2, a3: A3, a4: A4) => R): Curried4<A1, A2, A3, A4, R>;
-  <A1, A2, A3, A4, A5, R>(func: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R): Curried5<
-    A1,
-    A2,
-    A3,
-    A4,
-    A5,
-    R
-  >;
-
+  <A1, A2, A3, A4, A5, R>(func: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R): Curried5<A1,A2,A3,A4,A5,R>;
+  <A1, A2, A3, A4, A5,A6, R>(func: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5,a6:A6) => R): Curried6<A1,A2,A3,A4,A5,A6,R>;
   (func: (...a: any[]) => any): (...args: any[]) => any;
 }
