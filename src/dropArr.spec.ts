f69ed453a2dbb8eda0bcfcde6af16aa67a531799
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-12 16:09:12 -0400

    add Take

---
+++ src/dropArr.spec.ts
@@ -0,0 +1,34 @@
+import {flow} from './flow'
+import {dropArr} from './dropArr'
+import {createCounter} from './_internal/_counter'
+import {takeArr} from './takeArr'
+
+const array = [1, 2, 3, 4, 5]
+const expected = [3, 4, 5]
+
+describe('data first', () => {
+  test('should dropArr last', () => {
+    expect(dropArr(array, 2)).toEqual(expected)
+  })
+})
+
+describe('data last', () => {
+  test('dropArr', () => {
+    const result = flow(
+      array,
+      dropArr(2),
+    )
+    expect(result).toEqual(expected)
+  })
+  test('dropArr with take', () => {
+    const counter = createCounter()
+    const result = flow(
+      array,
+      counter.fn(),
+      dropArr(2),
+      takeArr(2),
+    )
+    expect(counter.count).toHaveBeenCalledTimes(4)
+    expect(result).toEqual([3, 4])
+  })
+})
---
+++ src/dropArr.ts
@@ -0,0 +1,59 @@
+// from https://raw.githubusercontent.com/remeda/remeda/master/src/dropArr.ts
+import {purry} from './purry'
+import {_reduceLazy, LazyResult} from './_internal/_reduceLazy'
+
+/**
+ * Removes first `n` elements from the `array`.
+ * @param array the target array
+ * @param n the number of elements to skip
+ * @signature
+ *    R.dropArr(array, n)
+ * @example
+ *    R.dropArr([1, 2, 3, 4, 5], 2) // => [1, 2, 3]
+ * @data_first
+ * @pipeable
+ * @category Array
+ */
+export function dropArr<T>(array: T[], n: number): T[]
+
+/**
+ * Removes last `n` elements from the `array`.
+ * @param array the target array
+ * @param n the number of elements to skip
+ * @signature
+ *    R.dropArr(n)(array)
+ * @example
+ *    R.dropArr(2)([1, 2, 3, 4, 5]) // => [1, 2, 3]
+ * @data_last
+ * @pipeable
+ * @category Array
+ */
+export function dropArr<T>(n: number): (array: T[]) => T[]
+
+export function dropArr() {
+  return purry(_dropArr, arguments, dropArr.lazy)
+}
+
+function _dropArr<T>(array: T[], n: number) {
+  return _reduceLazy(array, dropArr.lazy(n))
+}
+
+export namespace dropArr {
+  export function lazy<T>(n: number) {
+    let left = n
+    return (value: T): LazyResult<T> => {
+      if (left > 0) {
+        left--
+        return {
+          done: false,
+          hasNext: false,
+        }
+      }
+      return {
+        done: false,
+        hasNext: true,
+        next: value,
+      }
+    }
+  }
+}
