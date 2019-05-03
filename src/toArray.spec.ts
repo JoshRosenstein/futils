36e85a07c6f3ba9c0e94523a2ee05a04f436cc23
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-05-03 03:15:44 -0400

    types(toArray)

--- src/toArray.spec.ts
+++ src/toArray.spec.ts
@@ -1,4 +1,5 @@
 import toArray from './toArray'
+import { assertTrue, Equals } from 'typescript-test-utils'
 
 describe('toArray', () => {
   test('String', () => {
@@ -16,5 +17,40 @@ describe('toArray', () => {
     const a = toArray(1)
 
     expect(a).toEqual([1])
+  })
+
+  test('Number', () => {
+    const a = toArray(1)
+
+    expect(a).toEqual([1])
+  })
+
+
+})
+describe('Types', () => {
+  test('String', () => {
+    type e=string[]
+    const a = toArray('1')
+    const b = toArray(['1'])
+    assertTrue<Equals<typeof a, e>>()
+    assertTrue<Equals<typeof b, e>>()
+  })
+  test('tuple', () => {
+    type e='1'[]
+    const a = toArray('1' as '1')
+    const b = toArray(['1' as '1'])
+    assertTrue<Equals<typeof a, e>>()
+    assertTrue<Equals<typeof b, e>>()
+  })
+
+  const Cast = <T = any>(arg: any): T => arg;
+
+  test('Mixed', () => {
+    type u =  number | string | Record<string, any> | undefined | null
+    type e= u[]
+    const a = toArray<u>(Cast<u>(1))
+    const b = toArray([Cast<u>(1)])
+    assertTrue<Equals<typeof a, e>>()
+    assertTrue<Equals<typeof b, e>>()
   })
 })
--- src/toArray.ts
+++ src/toArray.ts
@@ -1,6 +1,7 @@
 import {isNil, isArray} from 'typed-is'
 
-export const toArray_ = v => (isNil(v) ? [] : isArray(v) ? v : [v])
+export const toArray_ = <T>(v: T[] | T | undefined | null): T[] =>
+  isNil(v) ? [] : isArray(v) ? v : [v]
 
 export const toArray = toArray_
 
